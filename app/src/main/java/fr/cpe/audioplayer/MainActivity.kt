71fcce9c9bbd11675eaac39c2c8595b33675f1cc
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2020-02-10 15:50:39 +0100

    feat(): add data binding to main activity

--- app/src/main/java/fr/cpe/audioplayer/MainActivity.kt
+++ app/src/main/java/fr/cpe/audioplayer/MainActivity.kt
@@ -2,11 +2,15 @@ package fr.cpe.audioplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import fr.cpe.audioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
